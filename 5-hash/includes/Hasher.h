39d7a1eb5740781caafdf4029f97525206c21077
Jan Lesek <honzalesek1996@gmail.com>
2021-04-27 17:56:47 +0200

    added some structures

--- 5-hash/includes/Hasher.h
+++ 5-hash/includes/Hasher.h
@@ -8,13 +8,32 @@
 
 #include <string>
 #include <utility>
+#include <vector>
+/**
+ * stands for file void, is set as an empty character for file input
+ */
+#define FVOID '~'
 
 class Hasher {
+
 private:
+    const std::vector<std::vector<bool>> VECTORS{
+    /*A*/  {0, 0, 1, 0,    1, 1, 1, 0,     1, 1, 0, 0,     0, 0, 0, 0},
+    /*B*/  {1, 1, 1, 0,    0, 0, 1, 1,     1, 0, 1, 1,     1, 1, 0, 0},
+    /*C*/  {1, 1, 1, 1,    1, 1, 0, 1,     0, 0, 0, 1,     0, 1, 1, 1},
+    /*D*/  {1, 0, 1, 0,    0, 0, 0, 1,     0, 0, 1, 1,     1, 1, 1, 1}
+    };
+//    const int8_t VECTORS_SIZE = (int8_t) VECTORS.size();
+/**
+ * bits per block
+ */
+    const int8_t BITSpBLOCK = 4;
+
     const std::string fileName{};
     unsigned long filesSize = 0;
 public:
-    explicit Hasher(char* fileName);
+
+    explicit Hasher(char *fileName);
 
     int hash();
 
--- 5-hash/src/Hasher.cpp
+++ 5-hash/src/Hasher.cpp
@@ -22,8 +22,28 @@ int Hasher::hash() {
         std::cout << "Hasher not initialised properly." << std::endl;
         return 2;
     }
+    std::ifstream input{};
+    input.open(fileName, std::ios::binary | std::ios::in);
+    if (!input.is_open())
+        throw std::runtime_error("Invalid filename on hash, an odd error!");
 
 
+
+    char buff[2];
+    while (!input.read(buff, 2).eof()) {
+        std::cout << buff;
+
+
+
+
+        std::fill_n(buff, 2, FVOID);
+    }
+
+    for (const char &b : buff){
+        if (b == FVOID)
+            break;
+        std::cout << b;
+    }
 
     return 0;
 }
